131296c1660d5c7ac776f9dd4db04399709f6665
Hamed Mamdoohi <hamed.mamdoohi@gmail.com>
2021-07-04 20:49:24 -0400

    fix(babel): add regenerator-runtime

--- babel.config.js
+++ babel.config.js
@@ -3,7 +3,14 @@ require('regenerator-runtime/runtime');
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: ['babel-preset-expo'],
+    presets: [
+      'babel-preset-expo',
+      {
+        useBuiltIns: 'entry',
+        corejs: 3,
+      },
+    ],
+
     plugins: ['react-native-reanimated/plugin'],
   };
 };
